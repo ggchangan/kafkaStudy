7c7a55cb77287174b22fd0865be393bbb0042670
ggchangan <xueluowuhen.2007@163.com>
2017-07-26 18:07:49 +0800

    add low level kakfa stream

---
+++ src/main/java/kafka/stream/CustomStreamConfig.java
@@ -0,0 +1,8 @@
+package kafka.stream;
+
+
+public class CustomStreamConfig {
+    public static final String HOST = "magneto7020:9092";
+    public static final String SOURCE_TOPIC = "TextLinesTopic";
+    public static final String SINK_TOPIC = "WordsWithCountsTopic";
+}
